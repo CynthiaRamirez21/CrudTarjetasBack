2d0f8f477190a724a73f52b12e88c6d2baca5e09
CynthiaRamirez21 <cynthiafrancini@gmail.com>
2022-10-21 16:19:45 -0300

    first commit

---
+++ BKTarjeta/AplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using BKTarjeta.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BKTarjeta
+{
+    public class AplicationDbContext : DbContext
+    {
+       public DbSet<TarjetaCredito> TarjetaCredito { get; set; }
+
+        public AplicationDbContext(DbContextOptions<AplicationDbContext> options) : base(options)
+        {
+
+        }
+
+    }
+}
---
+++ BKTarjeta/Migrations/20221005220105_v1.0.1.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BKTarjeta.Migrations
+{
+    public partial class v101 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "NumeroTarjeta",
+                table: "TarjetaCredito",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FechaExpiracion",
+                table: "TarjetaCredito",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CVV",
+                table: "TarjetaCredito",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "NumeroTarjeta",
+                table: "TarjetaCredito",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "FechaExpiracion",
+                table: "TarjetaCredito",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CVV",
+                table: "TarjetaCredito",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
